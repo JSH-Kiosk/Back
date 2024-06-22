7961083ed197cb669a39eec53f95d1957c83d84a
isayaksh <isayaksh@gmail.com>
2024-06-22 18:59:06 +0900

    <feat #1> User 도메인 디렉토리와 레이어 생성

---
+++ src/main/java/com/example/Kiosk/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.Kiosk.user.repository;
+
+import com.example.Kiosk.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/com/example/Kiosk/user/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.Kiosk.user.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+
+
+
+}
