b7b1dd96c7e8a3e7bbb144181cd77e34c3520b86
isayaksh <isayaksh@gmail.com>
2024-06-29 15:21:28 +0900

    <feat #5> OrderItemOption 도메인 생성

---
+++ src/main/java/com/example/Kiosk/order_item_option/repository/OrderItemOptionRepository.java
@@ -0,0 +1,7 @@
+package com.example.Kiosk.order_item_option.repository;
+
+import com.example.Kiosk.order_item_option.entity.OrderItemOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemOptionRepository extends JpaRepository<OrderItemOption, Long> {
+}
