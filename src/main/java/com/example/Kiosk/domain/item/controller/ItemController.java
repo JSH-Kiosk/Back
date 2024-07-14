21bc65d2a339db712cb6a3aa1def7436f0498430
isayaksh <isayaksh@gmail.com>
2024-07-14 17:20:11 +0900

    <feat #10> 단일 상품 조회 API

--- src/main/java/com/example/Kiosk/domain/item/controller/ItemController.java
+++ src/main/java/com/example/Kiosk/domain/item/controller/ItemController.java
@@ -1,9 +1,11 @@
 package com.example.Kiosk.domain.item.controller;
 
 import com.example.Kiosk.domain.item.dto.ItemAllSearchRes;
+import com.example.Kiosk.domain.item.dto.ItemSearchRes;
 import com.example.Kiosk.domain.item.service.ItemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,11 @@ public class ItemController {
     @GetMapping()
     public ResponseEntity<ItemAllSearchRes> getItemAllSearch() {
         return ResponseEntity.ok(new ItemAllSearchRes(new ArrayList<>()));
+    }
+
+    @GetMapping("/{itemId}")
+    public ResponseEntity<ItemSearchRes> getItemSearch(@PathVariable("itemId") Long itemId) {
+        return ResponseEntity.ok(new ItemSearchRes(0L, "name", "description", 0L, new ArrayList<>()));
     }
 
 
